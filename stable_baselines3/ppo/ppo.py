ac285250377f35bd7ec3ceb86a44e516bf34c3f5
Seongjin Choi <benchoi93@gmail.com>
2021-11-12 10:53:03 +0900

    Update ppo.py

--- stable_baselines3/ppo/ppo.py
+++ stable_baselines3/ppo/ppo.py
@@ -155,8 +155,9 @@ class PPO(OnPolicyAlgorithm):
         super(PPO, self)._setup_model()
 
         # Initialize schedules for policy/value clipping
-        if len(self.clip_range) == 0:
-            self.clip_range = 0.2
+        if not isinstance(self.clip_range, (float, int)):
+            if len(self.clip_range) == 0:
+                self.clip_range = 0.2
         self.clip_range = get_schedule_fn(self.clip_range)
         if self.clip_range_vf is not None:
             if isinstance(self.clip_range_vf, (float, int)):
